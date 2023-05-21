09d46a8d9060ef76b0e8587bf5febcc06ba6de5e
jjkip <jjlangatjose@gmail.com>
2023-05-22 00:13:59 +0300

    HStack,VStack and Zstack introduction

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,24 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Text("1")
-                Text("2")
-                Text("3")
-            }
-            HStack {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            HStack {
-                Text("7")
-                Text("8")
-                Text("9")
+        NavigationView{
+            VStack {
+                VStack(spacing: 20) {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                    Spacer()
+                }
+                VStack(alignment: .leading) {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                    Spacer()
+                }
+                ZStack{
+                    //Zstack is better used when we have  text on top of an image
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
+                HStack {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
+                HStack {
+                    Text("4")
+                    Text("5")
+                    Text("6")
+                }
+                HStack {
+                    Text("7")
+                    Text("8")
+                    Text("9")
+                }
             }
         }
-        .padding()
+        .navigationTitle("Stacks")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
